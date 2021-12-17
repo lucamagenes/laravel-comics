aaed1e7d947302cb8e7a4920045f7582f75a3abd
lucamagenes <lucamgns33@gmail.com>
2021-12-17 17:50:27 +0100

    add card to comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,8 +3,22 @@
 @section('page-title', 'Comics')
 
 @section('content')
+
+<div class="jumbo"></div>
 <div class="container">
-    <h1>FUMETTI</h1>
+    <div class="title">
+        <h1>CURRENT SERIES</h1>
+    </div>
+    <div class="row">
+        @foreach($comics as $comic)
+            <div class="col-2">
+                <div class="card h-100">
+                    <img src="{{$comic['thumb']}}" alt="">
+                    <h5 class="py-2">{{$comic['series']}}</h5>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 
 
