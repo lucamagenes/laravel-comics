3aacb9c6d4fdbffae9fbb095e2e325fa886e355a
lucamagenes <lucamgns33@gmail.com>
2021-12-19 18:08:04 +0100

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,101 @@
+<div id="site_footer">
+    <div class="top_footer">
+        <div class="container">
+            <div class="row">
+                <div class="contacts_footer col-6">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="menu_wrapper">
+                                <h4>DC COMICS</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="">Characters</a></li>
+                                    <li><a href="">Comics</a></li>
+                                    <li><a href="">Movies</a></li>
+                                    <li><a href="">TV</a></li>
+                                    <li><a href="">Games</a></li>
+                                    <li><a href="">Videos</a></li>
+                                    <li><a href="">News</a></li>
+                                </ul>
+                                <h4>SHOP</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="">Shop DC</a></li>
+                                    <li><a href="">Shop DC Collectibles</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="menu_wrapper">
+                                <h4>DC</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="">Terms Of Use</a></li>
+                                    <li><a href="">Privacy policy (New)</a></li>
+                                    <li><a href="">Ad Choice</a></li>
+                                    <li><a href="">Advertising</a></li>
+                                    <li><a href="">Jobs</a></li>
+                                    <li><a href="">Subscriptions</a></li>
+                                    <li><a href="">Talent Workshop</a></li>
+                                    <li><a href="">CPSC Certificates</a></li>
+                                    <li><a href="">Ratings</a></li>
+                                    <li><a href="">Shop Help</a></li>
+                                    <li><a href="">Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="menu_wrapper">
+                                <h4>SITES</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="">DC</a></li>
+                                    <li><a href="">MAD Magazine</a></li>
+                                    <li><a href="">DC Kids</a></li>
+                                    <li><a href="">DC Universe</a></li>
+                                    <li><a href="">DC Power Visa</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="copyright">
+                        <p>
+                            All Site Content TM and &copy 2020 DC Entertainment, unless otherwise <span class="copy_blue">noted here</span>. All rights reserved. <br>
+                            <span class="copy_blue">Cookies Settings</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="logo_footer col-6"></div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom_footer py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="sign_up">
+                        <button>
+                            SIGN-UP NOW!
+                        </button>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="socials d-flex">
+                        <h4>FOLLOW US</h4>
+                        <div class="social_icon">
+                            <img src="{{asset('img/footer-facebook.png')}}" alt="">
+                        </div>
+                        <div class="social_icon">
+                            <img src="{{asset('img/footer-twitter.png')}}" alt="">
+                        </div>
+                        <div class="social_icon">
+                            <img src="{{asset('img/footer-youtube.png')}}" alt="">
+                        </div>
+                        <div class="social_icon">
+                            <img src="{{asset('img/footer-periscope.png')}}" alt="">
+                        </div>
+                        <div class="social_icon">
+                            <img src="{{asset('img/footer-pinterest.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,8 +16,11 @@
     <div class="bottom_header py-4">
         <div class="container">
             <div class="row justify-content-center align-items-center">
+                <div class="col">
                     @include('partials.logo')
+                </div>
 
+                <div class="col">
                     <nav class="nav justify-content-center">
                         <a href="" class="nav-link">CHARACTERS</a>
                         <a href="{{ route('comics') }}" class="nav-link">COMICS</a>
@@ -30,10 +33,13 @@
                         <a href="" class="nav-link">NEWS</a>
                         <a href="" class="nav-link">SHOP</a>
                     </nav>
+                </div>
 
+                <div class="col">
                     <div class="search text-end">
                         <p>search</p>
                     </div>
+                </div>
             </div>
         </div>
     </div>
